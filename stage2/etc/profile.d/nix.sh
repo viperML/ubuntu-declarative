58e8a418fa1117d81c0898d44183be8ec45eed01
Fernando Ayats <ayatsfer@gmail.com>
2022-06-04 23:19:13 +0200

    misc tweaks

---
+++ stage2/etc/profile.d/nix.sh
@@ -0,0 +1,3 @@
+export NIX_REMOTE=daemon
+export PATH="$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH"
+export NIX_PATH="nixpkgs=/nix/var/nix/profiles/per-user/$USER/channels/nixpkgs:/nix/var/nix/profiles/per-user/$USER/channels"
