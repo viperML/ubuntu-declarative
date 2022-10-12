64437b08f135ebdbf6c16322d1e45989dd0392c7
Fernando Ayats <ayatsfer@gmail.com>
2022-10-12 21:32:26 +0200

    update ubu7

--- bin/apply.sh
+++ bin/apply.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 DIR=$(cd "$(dirname "$0")";cd ..; pwd)
 
-stages=(stage1 stage2)
+stages=(stage1 stage2 stage3)
 
 for s in ${stages[@]}; do
     dpkg-deb --build --root-owner-group $DIR/$s $s.deb
@@ -13,4 +13,5 @@ apt install --reinstall --yes ./stage1.deb
 apt update
 apt upgrade --yes
 apt install --reinstall --yes ./stage2.deb
+apt install --reinstall --yes ./stage3.deb
 apt autopurge
